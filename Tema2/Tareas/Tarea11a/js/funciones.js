e129951279cd8ee853f2d6d3ff0e2d08e1663fde
guzman <guzmanghu@gmail.com>
2022-11-09 08:48:31 +0100

    casa

--- Tema2/Tareas/Tarea11a/js/funciones.js
+++ Tema2/Tareas/Tarea11a/js/funciones.js
@@ -1,12 +1,22 @@
  let velocidad=10;
  let xDiana=50;
+ const element =document.getElementById('diana');
+ let ancho = element.scrollWidth;
+ console.log(ancho);
+
  function desplazarDiana() {
-    xDiana+=velocidad;
-    document.getElementById('div1').style.left=`${xDiana}px`;
-    // this.left=`$velocidad`
+   if (xDiana >= 40) {
+      xDiana+=velocidad;
+      document.getElementById('div1').style.marginLeft =`${xDiana}px`;
+   }else if(xDiana = ancho){
+      velocidad=vlocidad*(-1);
+   }
+   
+
+   
  }
 
 let interval=setInterval(desplazarDiana,50);
 
 
- //document.getElementById('div3').addEventListener('load',mover);
+//document.getElementById('div3').addEventListener('load',desplazarDiana);
