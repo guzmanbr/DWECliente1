9bc7515375dea17a8fdd85d516ee5b974d271fad
guzmanbr <guzmanghu@gmail.com>
2022-11-10 11:50:49 +0100

    clase

--- Tema2/Tareas/Tarea11a/js/funciones.js
+++ Tema2/Tareas/Tarea11a/js/funciones.js
@@ -1,22 +1,26 @@
- let velocidad=10;
- let xDiana=50;
- const element =document.getElementById('diana');
- let ancho = element.scrollWidth;
- console.log(ancho);
-
- function desplazarDiana() {
-   if (xDiana >= 40) {
+   let velocidad=10;
+   let xDiana=0;
+   let diametreoDiana=60;
+   const element = document.getElementById('diana');
+   //let final = element.clientEidth;
+   let final = (document.documentElement.clientWidth)-diametreoDiana;
+   console.log(final);
+   function desplazarDiana() {
       xDiana+=velocidad;
       document.getElementById('div1').style.marginLeft =`${xDiana}px`;
-   }else if(xDiana = ancho){
-      velocidad=vlocidad*(-1);
+      //Cuando la diana llega al final da la vuelta
+      if (xDiana >= final) {
+         velocidad=velocidad*(-1);
+      }
+      //Cuando la diana llega al principio vuelve a girar
+      if (xDiana <= 0) {
+         velocidad=velocidad*(-1);
+      }
    }
    
 
-   
- }
+   function comenzar() {
+      const intervalDiana = setInterval(desplazarDiana,20);
+   }
 
-let interval=setInterval(desplazarDiana,50);
-
-
-//document.getElementById('div3').addEventListener('load',desplazarDiana);
+   document.addEventListener('load',comenzar());
