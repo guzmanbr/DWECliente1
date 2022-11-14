0f3dcfa03bc8ae4adaeee28bc530f0bfc0144d51
guzmanbr <guzmanghu@gmail.com>
2022-11-14 14:22:26 +0100

    clase

---
+++ Tema2/Ejercicios/Arraydivs/js/funciones.js
@@ -0,0 +1,36 @@
+
+let arrayCuadrados=[];
+
+function crearCuadrado(idCuadrado,lado,fondo) {
+    let cuadrado = document.createElement('div');
+    //let texto = document.createTextNode('Palabras....');
+    //cuadrado.appendChild(texto);
+    cuadrado.setAttribute('id',idCuadrado);
+    cuadrado.setAttribute('style',`width: ${lado}px; background-color: ${fondo}`);
+    cuadrado.style.height=`${lado}px`;
+    cuadrado.style.margin='10px';
+    cuadrado.setAttribute('style',`${cuadrado.getAttribute('style')}; float: left;`)
+    //añadimos al documento
+    document.body.appendChild(cuadrado);
+}
+
+// crearCuadrado('cuadrado1',25,'blue');
+
+
+
+for (let i = 0; i < 25; i++) {
+   arrayCuadrados[i] = crearCuadrado(`cuadrado${i}`,50,'blue');
+}
+
+// for (indice of arrayCuadrados){
+//     if (indice % 2 === 0) {
+//         arrayCuadrados[indice].style.backgroundColor='red';
+//     }
+// }
+
+for (let i = 0; i < arrayCuadrados.length; i++) {
+    if (i % 2 === 0) {
+        arrayCuadrados[i].style.backgroundColor='red';
+    }
+ }
+ 
